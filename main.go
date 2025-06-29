88871953742581caccc6b7d4b57926a394fee4fc
lynetma2 <lynetma2@users.noreply.github.com>
2025-06-30 00:50:40 +0200

    Building types needed

--- main.go
+++ main.go
@@ -18,4 +18,52 @@ func main() {
 		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
 		fmt.Println("i =", 100/i)
 	}
+
+}
+
+// Types i might need.
+type Terrain struct {
+	location   [2]int
+	kind       TerrainKind
+	diceNumber int
+	tradeKind  TerrainKind
+}
+
+type Road struct {
+	location [3]int
+	player   int
+}
+
+type VerticesPiece struct {
+	kind     PieceKind
+	location [3]int
+	player   int
+}
+
+type TerrainKind int
+
+const (
+	Lumber TerrainKind = iota
+	Brick
+	Wool
+	Grain
+	Ore
+	Dessert
+	Port
+	Sea
+	Resource
+)
+
+type PieceKind int
+
+const (
+	Settlement PieceKind = iota
+	City
+)
+
+type Board struct {
+	terrain []Terrain
+	roads   []Road
+	pieces  []VerticesPiece
+	thief   [2]int
 }
