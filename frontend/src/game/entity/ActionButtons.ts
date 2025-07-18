d8f7abc7395704ecacf54d65dba91f99c089abab
sund <99sund@gmail.com>
2025-07-18 23:29:27 +0200

    Working on ActionButtons.ts

--- frontend/src/game/entity/ActionButtons.ts
+++ frontend/src/game/entity/ActionButtons.ts
@@ -1,3 +1,5 @@
+import {GameEvent} from "@/game/GameEvent.ts";
+import {CitySVGString, DevelopmentCardBackSVGString, HouseSVGString} from "@/assets/assets.tsx";
 
 export class ActionButton {
 
@@ -10,19 +12,44 @@ export class ActionButton {
     public draw(canvas: HTMLCanvasElement, x: number) {
         const buttonHeight = 80;
         const buttonWidth = 80;
+        const iconWidth = 40;
+        const iconHeight = 40;
         const y = 900;
         const ctx = canvas.getContext('2d');
+        const image = new Image();
 
         if (!ctx) {
             console.error("Can't draw action button");
             return;
         }
 
+        switch (this.kind) {
+            case GameEvent.TRADE:
+                //TODO find icon for this.
+                break;
+            case GameEvent.PUTCITY:
+                image.src = CitySVGString(iconWidth, iconHeight, "yellow");
+                break;
+            case GameEvent.PUTSETTLEMENT:
+                image.src = HouseSVGString(iconWidth, iconHeight, "yellow");
+                break;
+            case GameEvent.DRAWDEVELOPMENTCARD:
+                image.src = DevelopmentCardBackSVGString(iconWidth, iconHeight, "yellow");
+                break;
+            case GameEvent.PUTROAD:
+                //TODO draw something for this.
+                break;
+        }
+
+
         ctx.fillStyle = "white";
         ctx.strokeStyle = "yellow";
-
+        ctx.beginPath();
         ctx.fillRect(x,y, buttonWidth, buttonHeight);
         ctx.strokeRect(x, y, buttonWidth, buttonHeight);
+        ctx.closePath();
+
+        ctx.drawImage(image, x+iconWidth/2, y+iconHeight/2);
 
         ctx.strokeStyle = "white";
     }
