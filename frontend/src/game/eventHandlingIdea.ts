7d418a15f140c889b03a6e6e47f15eda8d634f06
lynetma2 <lynetma2@users.noreply.github.com>
2025-09-06 20:59:18 +0200

    Progress on the internal interaction and event systems

--- frontend/src/game/eventHandlingIdea.ts
+++
@@ -1,9 +0,0 @@
-import type {MouseEvent} from "react";
-
-//Should maybe have the canvas as an internal field.
-//Keep size etc as fields.
-interface Button {
-    draw(canvas: HTMLCanvasElement): void;
-    onClick(e: MouseEvent, canvas: HTMLCanvasElement, callback:() => void): void; //Should redraw itself
-    onHover(e: MouseEvent, canvas: HTMLCanvasElement, callback:() => void): void; //Should redraw itself
-}
